7b13a13dc9f27da63ea77599f6a229160ba99456
Gyeongjun Kim <lastpuzly@gmail.com>
2021-03-08 17:27:44 +0900

    chore: init survey service module

---
+++ survey-service/src/test/java/com/ktpt/surmoon/service/survey/SurveyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktpt.surmoon.service.survey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
