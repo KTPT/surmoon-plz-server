9023effb99edf063ab61be459fb46c501beec14f
toneyparky <qkrtmddhks95@gmail.com>
2021-03-16 23:02:18 +0900

    refactor: 불필요한 부분 수정

--- survey-service/src/main/java/com/ktpt/surmoon/service/survey/config/SecurityConfig.java
+++ survey-service/src/main/java/com/ktpt/surmoon/service/survey/config/SecurityConfig.java
@@ -10,47 +10,15 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final CustomOAuth2UserService customOAuth2UserService;
-
     @Override
     protected void configure(HttpSecurity security) throws Exception {
-        // security
-        //     .authorizeRequests()
-        //     .antMatchers(
-        //         "/",
-        //         "/error",
-        //         "/favicon.ico",
-        //         "/**/*.png",
-        //         "/**/*.gif",
-        //         "/**/*.svg",
-        //         "/**/*.jpg",
-        //         "/**/*.html",
-        //         "/**/*.css",
-        //         "/**/*.js"
-        //     )
-        //     .permitAll();
-        // security
-        //     .authorizeRequests()
-        //     .anyRequest().authenticated()
-        //     .and()
-        //     .formLogin()
-        //     .loginPage("/login")
-        //     .loginProcessingUrl("/login")
-        //     .defaultSuccessUrl("/")
-        //     .failureUrl("/login/form?error")
-        //     .permitAll();
-
         security
             .authorizeRequests()
             .antMatchers("/", "/login/**", "/logout", "/error", "/h2-console/**").permitAll()
             .antMatchers("/hi", "/login-success").authenticated()
-            .antMatchers("/**").denyAll()
+            .anyRequest().authenticated()
             .and()
             .oauth2Login()
-            .loginPage("/hi")
-            .userInfoEndpoint()
-            .userService(customOAuth2UserService)
-            .and()
             .defaultSuccessUrl("/login-success", true);
 
         security
